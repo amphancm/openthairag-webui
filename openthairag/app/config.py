d18cc73c75ed46c96d685dc11771253d700f3214
Adun Nantakaew <amphancm@gmail.com>
2025-05-16 14:51:01 +0700

    Update config.py

--- openthairag/app/config.py
+++ openthairag/app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/openthairag")
+    MONGO_URI  = os.getenv("MONGO_URI", "mongodb://localhost:27017/openthairag")
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
